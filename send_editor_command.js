c490a0056ec5d4de361b8dadb463361d6f00b4c1
lovebird <lovebirdsx@gmail.com>
2023-02-13 12:41:19 +0800

    适应新端口变化

--- send_editor_command.js
+++ send_editor_command.js
@@ -4,6 +4,6 @@ import { Buffer } from 'buffer';
 const myArgs = process.argv.slice(2);
 const message = myArgs.length <= 0 ? Buffer.from('Hello World') : myArgs.join(' ');
 const client = dgram.createSocket('udp4');
-client.send(message, 8889, 'localhost', (err) => {
+client.send(message, 8890, 'localhost', (err) => {
   client.close();
 });
